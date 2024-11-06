0a5ad2d7adc448db8f550d04d8f85e601325ad18
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-06 14:23:03 +0000

    variables

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Color CurrentClr = Colors.white;
+  TextEditingController myController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
