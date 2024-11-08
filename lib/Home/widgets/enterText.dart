51619c968b541bc38ee45da95ba2e460e03385a5
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-08 15:37:58 +0000

    Text widget

---
+++ lib/Home/widgets/enterText.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TextEnter extends StatelessWidget {
+  const TextEnter({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Enter Hex Code to change backGround Color',
+      style: TextStyle(fontSize: 16),
+    );
+  }
+}
