65bbe23a0947a4c928c9b9b40c159a22415e88a9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-10 15:52:30 +0000

    blue container

--- lib/Home/HomePage.dart
+++ lib/Home/HomePage.dart
@@ -1,4 +1,5 @@
 
+import 'package:colorgenerate/Home/widgets/BlueContainer.dart';
 import 'package:colorgenerate/Home/widgets/TextField.dart';
 import 'package:colorgenerate/Home/widgets/enterText.dart';
 import 'package:colorgenerate/consts/Colors.dart';
@@ -104,32 +105,6 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class BlueContainer extends StatelessWidget {
-  const BlueContainer({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 40,
-      width: MediaQuery.of(context).size.width * 0.97,
-      color: myColors.Cblue,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text("BLUE"),
-            Spacer(),
-            SelectableText("#0000FF")
-          ],
         ),
       ),
     );
