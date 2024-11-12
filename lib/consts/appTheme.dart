abe8687972a86a8004acd1352d17a3c4ca44422b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-12 15:06:34 +0000

    app theme

---
+++ lib/consts/appTheme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+ static final InputBorder myBorder = OutlineInputBorder(
+    borderRadius: const BorderRadius.all(
+      const Radius.circular(10.0),
+    ),
+  );
+  static final themeData = ThemeData().copyWith(
+    inputDecorationTheme: InputDecorationTheme(
+      border: myBorder,
+      focusedBorder: myBorder,
+      enabledBorder: myBorder,
+    )
+  );
+}
