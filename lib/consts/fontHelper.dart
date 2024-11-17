26cb43460b08256248036d116043493ec2516edd
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-17 15:35:12 +0000

    font helper

---
+++ lib/consts/fontHelper.dart
@@ -0,0 +1,9 @@
+
+
+ import 'package:flutter/material.dart';
+
+class Fonthelper {
+   static const FontWeight bold = FontWeight.w700;
+   static FontWeight thin = FontWeight.w100;
+   static FontWeight medium = FontWeight.w500;
+ }
