0fb2f53d9a2a41fb9cd7a0b02e06cc0afb3679fd
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-17 15:35:05 +0000

    font helper

--- lib/consts/textStyle.dart
+++ lib/consts/textStyle.dart
@@ -1,10 +1,11 @@
 
 
 import 'package:colorgenerate/consts/Colors.dart';
+import 'package:colorgenerate/consts/fontHelper.dart';
 import 'package:flutter/material.dart';
 
 TextStyle enter = TextStyle(
     fontSize: 16,
-  fontWeight: FontWeight.bold
+  fontWeight: Fonthelper.bold
 );
 TextStyle examples = TextStyle(fontSize: 14, color: myColors.exampleClr);
