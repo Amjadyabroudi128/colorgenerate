79dc554774737bb5d0774c9955c0299d395b76e1
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-17 15:23:18 +0000

    grey color

--- lib/consts/textStyle.dart
+++ lib/consts/textStyle.dart
@@ -1,9 +1,10 @@
 
 
+import 'package:colorgenerate/consts/Colors.dart';
 import 'package:flutter/material.dart';
 
 TextStyle enter = TextStyle(
     fontSize: 16,
   fontWeight: FontWeight.bold
 );
-TextStyle examples = TextStyle(fontSize: 14, color: Colors.grey.shade800);
+TextStyle examples = TextStyle(fontSize: 14, color: myColors.exampleClr);
