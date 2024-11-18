dc9b4d66fa3d075fe5aafd56af48d1a523f11262
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-11-18 12:19:17 +0000

    input border

--- lib/consts/appTheme.dart
+++ lib/consts/appTheme.dart
@@ -1,18 +1,14 @@
+import 'package:colorgenerate/consts/BorderRadius.dart';
 import 'package:flutter/material.dart';
 
 import 'Colors.dart';
 
 class AppTheme {
- static final InputBorder myBorder = OutlineInputBorder(
-    borderRadius: const BorderRadius.all(
-      const Radius.circular(10.0),
-    ),
-  );
   static final themeData = ThemeData().copyWith(
     inputDecorationTheme: InputDecorationTheme(
-      border: myBorder,
-      focusedBorder: myBorder,
-      enabledBorder: myBorder,
+      border: meBorder,
+      focusedBorder: meBorder,
+      enabledBorder: meBorder,
     ),
     textSelectionTheme: TextSelectionThemeData(
       cursorColor: myColors.cursor,
